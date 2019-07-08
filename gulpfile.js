3bd8983a0d0ae963965d9059233aa165cf36acd5
Cheng <gadiex@gmail.com>
2019-07-08 21:56:52 +0800

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,15 +12,16 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 
-gulp.task('coffee', function() {
+gulp.task('coffee', async function() {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
 });
 
-gulp.task('js', function() {
+gulp.task('js', async function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
